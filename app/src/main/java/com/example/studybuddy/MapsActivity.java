43f79d63cf586efcdb0c449167cb156f3b309f17
Ryan Kim <mck649@gmail.com>
2022-03-07 16:52:06 -0800

    Done Google Maps

--- app/src/main/java/com/example/studybuddy/MapsActivity.java
+++ app/src/main/java/com/example/studybuddy/MapsActivity.java
@@ -1,16 +1,16 @@
 package com.example.studybuddy;
 
-import androidx.fragment.app.FragmentActivity;
-
 import android.os.Bundle;
 
+import androidx.fragment.app.FragmentActivity;
+
+import com.example.studybuddy.databinding.ActivityMapsBinding;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.example.map.databinding.ActivityMapsBinding;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
